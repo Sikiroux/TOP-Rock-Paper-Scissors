453b9b9ba0376d66ddb261fb0b003bffe187bfb9
Damien Ekouma <damienekouma@gmail.com>
2022-10-26 12:19:12 -1000

    Add function to keep tracks of the score for both players

--- rps.js
+++ rps.js
@@ -16,6 +16,8 @@ function getComputerChoice() {
     }
     return compChoice;
 }
+
+let winner = "";
 // Playing one round of the game with two paramater playerSelection 
 // and computerSelection then return the winner. the function need to be case sensitive
 
@@ -24,23 +26,28 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerInput()
     console.log(playerSelection)
     console.log(computerSelection)
+    
     if (playerSelection === computerSelection) {
-        console.log(`Both player made the same move ${computerSelection}`);
+        winner = `Both player made the same move ${computerSelection}`;
         
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        console.log(`You chosed ${playerSelection} and won`);
+        winner = `You chosed ${playerSelection} and won`;
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        console.log(`You chosed ${playerSelection} and won`);
+        winner = `You chosed ${playerSelection} and won`;
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        console.log(`You chosed ${playerSelection} and won`);
+        winner = `You chosed ${playerSelection} and won`;
     }  else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        console.log(`You chosed ${playerSelection} and lost`);
+        winner = `You chosed ${playerSelection} and lost`;
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        console.log(`You chosed ${playerSelection} and lost`);
+        winner = `You chosed ${playerSelection} and lost`;
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        console.log(`You chosed ${playerSelection} and lost`);
+       winner = `You chosed ${playerSelection} and lost`;
     }
+    console.log(winner);
+    return winner;
+    
 }
+console.log(winner)
 
 //Get input from player through a prompt
 function playerInput() {
@@ -51,8 +58,16 @@ function playerInput() {
 
 //Function to play 5 rounds
 function game() {
+    let scorePlayer = 0
+    let scoreComputer = 0
     for (let i = 0; i < 5; i++) {
         playRound();
-        
+        if (winner.includes("won")) {
+            scorePlayer++
+        } else if (winner.includes("lost")) {
+            scoreComputer++
+        }
+        console.log(scorePlayer)
+        console.log(scoreComputer)
     }
 }
